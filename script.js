063eb41f72623c7ec04a4dc34f8da042a5c3af7d
Zoc Nishant <zocnishant@gmail.com>
2024-02-18 13:57:11 +0545

    first page done

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-const time = gsap.timeline;
+function loading() {
+  // Gsap Animation
+  const time = gsap.timeline();
 
-gsap.to(".yellow", {
-  top: "-100%",
-  delay: 0.5,
-  duration: 0.5,
-  ease: "expo.out",
+  time.to(".yellow", {
+    top: "-100%",
+    delay: 0.5,
+    duration: 0.7,
+    ease: "expo.out",
+  });
+
+  time.from(
+    ".yellow_second",
+    {
+      top: "100%",
+      delay: 0.5,
+      duration: 0.7,
+      ease: "expo.out",
+    },
+    "pasa"
+  );
+
+  time.to(
+    ".loader h1",
+    {
+      delay: 0.6,
+      duration: 0.7,
+      color: "black",
+    },
+    "pasa"
+  );
+
+  time.to(".loader", {
+    opacity: 0,
+  });
+
+  time.to(".loader", {
+    display: "none",
+  });
+}
+
+loading();
+
+//Locomotive
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
 });
